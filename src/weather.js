5080b67e3abcddc8fc5752342e93eacfedeaa593
Cydsthakyd <128433979+Cydsthakyd@users.noreply.github.com>
2023-06-14 19:40:15 -0400

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -75,6 +75,7 @@ function displayForecast(response) {
          `;
     })
 
+
     forcastHTML = forcastHTML + `</div>`;
     forcastElement.innerHTML = forcastHTML;
 }
@@ -106,7 +107,7 @@ function changeCity(event) {
 }
 function getForecast(coordinates) {
     let apiKey = `5aac6d0188c6f17d6d2bbe6591b6fef0`;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(displayForecast);
 }
 
