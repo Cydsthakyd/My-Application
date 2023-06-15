82438761ba958176cbcc925bef62bf4378f7464c
Cydsthakyd <128433979+Cydsthakyd@users.noreply.github.com>
2023-06-14 20:51:21 -0400

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -89,7 +89,7 @@ function displayForecast(response) {
                 alt="sunny cloud"
                 id="icon" />
               <div class="temp">
-                <span class="max" id="maxT">${Math.round(forcasterDay.temp.max)}°F</span>
+                <span class="max" id="maxT">${Math.round(forcasterDay.temp.max)}°F|</span>
                 <span class="min" id="lowT">${Math.round(forcasterDay.temp.min)}°F</span>
               </div>
             </div>
